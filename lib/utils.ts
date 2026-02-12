1fc3c2e8500814d854b57ea6a3c64ad1bdc64f25
saannndec5ty <stokakunsanzz250@gmail.com>
2026-02-12 18:08:46 +0700

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,20 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+  }).format(amount);
+}
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat('id-ID', {
+    dateStyle: 'medium',
+  }).format(date);
+}
